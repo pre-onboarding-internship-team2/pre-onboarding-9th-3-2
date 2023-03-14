486d18bfbfc89c1f60443e8010f7f64d2e634cbc
ggsno <ogs0518@gmail.com>
2023-03-15 00:03:54 +0900

    refactor: 파라미터 타입 수정 - 모킹데이터 기준 차트 -> 2가지 y축을 가진 범용적 차트로 리팩터링 - 재사용성 및 가독성을 위함

---
+++ src/components/TimeSeriesChart.tsx
@@ -0,0 +1,85 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  BarElement,
+  LogarithmicScale,
+} from "chart.js";
+import { Chart } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  LogarithmicScale,
+  Filler,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export type TimeSeriesProps = {
+  xAxisData: string[];
+  yAxisLeft: { name: string; data: number[] };
+  yAxisRight: { name: string; data: number[] };
+};
+
+export default function TimeSeriesChart({ props }: { props: TimeSeriesProps }) {
+  const { xAxisData, yAxisLeft, yAxisRight } = props;
+
+  return (
+    <Chart
+      options={{
+        interaction: {
+          mode: "index" as const,
+          intersect: false,
+        },
+        scales: {
+          area: {
+            type: "linear" as const,
+            display: true,
+            position: "left" as const,
+            title: { display: true, text: yAxisLeft.name },
+          },
+          bar: {
+            type: "linear" as const,
+            display: true,
+            position: "right" as const,
+            title: { display: true, text: yAxisRight.name },
+          },
+        },
+      }}
+      data={{
+        labels: xAxisData,
+        datasets: [
+          {
+            type: "line" as const,
+            yAxisID: "area",
+            fill: true,
+            label: yAxisLeft.name,
+            data: yAxisLeft.data,
+            borderColor: "#94b799",
+            backgroundColor: "#a3c2a880",
+          },
+          {
+            type: "bar" as const,
+            yAxisID: "bar",
+            label: yAxisRight.name,
+            data: yAxisRight.data,
+            borderColor: "#69bded",
+            backgroundColor: "#7fc7efaa",
+          },
+        ],
+      }}
+      type="line"
+    />
+  );
+}
