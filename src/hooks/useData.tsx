209a9d834ef7216eea9c5e0c2aeacba455cb1855
jiyeon <a1919361@gmail.com>
2023-03-14 12:44:21 +0900

    add : 차트 컴포넌트에서 필요한 데이터 요청하고 리턴하는 훅 작성

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react';
+import { ChartData } from 'chart.js';
+import { useEffect, useState } from 'react';
 import httpClient from '../api/httpClient';
+import { TimeSeriesChartDataType } from '../types/chartData.types';
+import { getChartData } from '../utils/getChartData';
 
-function useData() {
+function useChartData() {
+    const [chartData, setChartData] = useState<
+        ChartData<'bar' | 'line', TimeSeriesChartDataType[]>
+    >({
+        datasets: [],
+    });
+
     const loadData = async () => {
-        const data = await httpClient.loadData();
-        console.log(data);
+        const responseData = await httpClient.loadData();
+
+        if (!responseData) return;
+
+        setChartData(getChartData(responseData));
     };
 
     useEffect(() => {
         loadData();
     }, []);
+
+    return {
+        chartData,
+    };
 }
 
-export default useData;
+export default useChartData;
