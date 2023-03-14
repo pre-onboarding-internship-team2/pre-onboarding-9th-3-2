dca939d3ab007edeaa19510dc394f922a496b0c9
jiyeon <a1919361@gmail.com>
2023-03-15 04:48:01 +0900

    refactor : 데이터 로딩 state 추가, locations 데이터 추가

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -2,29 +2,39 @@ import { ChartData } from 'chart.js';
 import { useEffect, useState } from 'react';
 import httpClient from '../api/httpClient';
 import { TimeSeriesChartDataType } from '../types/chartData.types';
+import { LocationDataType } from '../types/response.types';
 import { getChartData } from '../utils/getChartData';
+import getUniqueLocations from '../utils/getUniqueLocations';
 
 function useChartData() {
+    const [isLoading, setIsLoading] = useState(false);
     const [chartData, setChartData] = useState<
         ChartData<'bar' | 'line', TimeSeriesChartDataType[]>
     >({
         datasets: [],
     });
 
-    const loadData = async () => {
-        const responseData = await httpClient.loadData();
-
-        if (!responseData) return;
-
-        setChartData(getChartData(responseData));
-    };
+    const [locations, setLocations] = useState<Array<LocationDataType['id']>>([]);
 
     useEffect(() => {
+        const loadData = async () => {
+            setIsLoading(true);
+            const responseData = await httpClient.loadData();
+
+            if (!responseData) return;
+
+            setLocations(getUniqueLocations(responseData));
+
+            setChartData(getChartData(responseData));
+            setIsLoading(false);
+        };
         loadData();
     }, []);
 
     return {
         chartData,
+        locations,
+        isLoading,
     };
 }
 
